ca54ed55f2230cd57437cd372d9a2596d9612532
xavier cordova <xaviercordovajr@gmail.com>
2021-06-09 12:24:58 -0500

    Update main.tf
    Add a bucket to main.tf to test the Terraform-apply job in github workflow. Bucket will be deleted after test.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,15 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "ex-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "delete after test"
+  }
+}
+
 ## Import Hosted Zone Data ##
 data "aws_route53_zone" "resume" {
   name         = "xaviercordovajr.com."
